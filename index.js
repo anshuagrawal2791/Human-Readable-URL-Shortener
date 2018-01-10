4dc93ebc24fad9d9e1c1e17e8524bd8c9d564f45
Anshuman Agrawal <anshuagrawal2791@gmail.com>
2018-01-10 15:49:21 +0530

    added custom code feature

--- index.js
+++ index.js
@@ -4,78 +4,95 @@ const mongoose = require('mongoose');
 var app = express();
 var urlSchema = require('./urls.js');
 const rand = require('csprng');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.get('/new/*',(req,res)=>{
-  // console.log(req);
+app.get('/new/*', (req, res) => {
     var url = req.url;
-    url = url.replace('/new/','');
+    var customCode = '';
+    //replace the keyword 'new'
+    url = url.replace('/new/', '');
+
     var isValid = validate(url);
-    if(!isValid)
-      res.json({'message':'invalid url'});
-  else{
-    console.log(url);
-    createUrl(url,req,(err,data)=>{
-        if(err)
-        res.sendStatus(500);
-        var response = {};
-        response.original_url=url;
-        response.short_url=data;
-        res.json(response);
-    });
-  }
+    // send 400 if url is invalid
+    if (!isValid)
+        res.send(400, 'invalid url');
+    else {
+        // check if custom code is provided
+        if (url.indexOf('code') != -1) {
+
+            // if provided, extract the code
+            customCode = url.substring(url.indexOf('code')).split('=')[1];
+            //remove code from url
+            url = url.substring(0, url.indexOf('code') - 1);
+
+        }
+        createUrl(url, customCode, req, (err, data) => {
+            if (err) {
+                // if code already exists
+                res.status(400);
+                res.send('Code provided already exists. Please try with another code');
+            } else {
+                var response = {};
+                response.original_url = url;
+                response.short_url = data;
+                res.json(response);
+            }
+        });
+    }
 });
 
-var validate = (url)=>{
-  var res = url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-    if(res == null)
+var validate = (url) => {
+    var res = url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    if (res == null)
         return false;
     else
         return true;
 }
 
-app.get('/*',(req,res)=>{
+// go to the shortened url
+app.get('/*', (req, res) => {
     var url = req.params['0'];
-  // console.log(req.params);
     var code = url.split('/').pop();
-    // console.log(code);
-  urlSchema.findOne({'code':code},(err,entry)=>{
-    console.log(entry);
-        if(err||!entry)
-        res.json({'message':'url not found'});
-        else{
-          var redirectTo = entry.original;
-          if (!/^https?:\/\//i.test(redirectTo)) {
-            redirectTo = 'https://' + redirectTo;
-          }
-          console.log(redirectTo);
-          res.redirect(''+redirectTo);
+    urlSchema.findOne({ 'code': code }, (err, entry) => {
+        if (err || !entry)
+            res.json({ 'message': 'url not found' });
+        else {
+            var redirectTo = entry.original;
+            if (!/^https?:\/\//i.test(redirectTo)) {
+                redirectTo = 'https://' + redirectTo;
+            }
+            res.redirect('' + redirectTo);
         }
-        
-  });
+
+    });
 });
 
 
 
 
 
-var createUrl=(url,req,callback)=>{
-  
+var createUrl = (url, customCode, req, callback) => {
+    var codeToPut = (customCode) ? customCode : rand(24, 24);
     var newurl = new urlSchema({
-        original:url,
-        createdAt:new Date(),
-        code:rand(24,24)
+        original: url,
+        createdAt: new Date(),
+        code: codeToPut
     });
-    newurl.save((err,resp)=>{
-      console.log(resp);
-        if(err){
+    newurl.save((err, resp) => {
+        console.log(resp);
+        if (err) {
             return callback(err);
         }
-        callback(null,req.headers.host+'/'+resp.code);
+        callback(null, req.headers.host + '/' + resp.code);
     });
 }
 
 
-app.listen(process.env.PORT,()=>{
-    mongoose.connect('mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+process.env.DB_HOST);
-    console.log('listening on port: '+process.env.PORT);
+app.listen(process.env.PORT, () => {
+    mongoose.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + process.env.DB_HOST);
+    console.log('listening on port: ' + process.env.PORT);
 });
--- models/urls.js
+++ models/urls.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 // var code = rand(24,24);
 var url = mongoose.Schema({
     original:String,
-    code:String,
+    code:{type:String,unique:true},
     created_at:Date
 });
 module.exports = mongoose.model('URLs',url);
+
